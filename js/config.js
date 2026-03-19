dd131b8061aa6f4f30d999f310b7b20821d6a0c1
pkr <rajarajeshwari9344534@gmail.com>
2026-03-19 18:43:29 +0530

    add: New BASE_URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,6 @@
-const BASE_URL = "https://justice-nest-backend.onrender.com";
+// const BASE_URL = "https://justice-nest-backend.onrender.com";
+
+// const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://justice-nest-bac-git-92c865-rajarajeshwari9344534-rgbs-projects.vercel.app"
 
 export default BASE_URL;
